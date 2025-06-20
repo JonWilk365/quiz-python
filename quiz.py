b11358428eaee6a23d9388a4b6ce501a20ed7193
JonWilk365 <jonataswilkinson@gmail.com>
2025-06-20 12:50:55 -0300

    inicio implementação quiz

---
+++ quiz.py
@@ -0,0 +1,4 @@
+print("Seja bem-vindo ao Quiz de Direito Constitucional, Direito Civil e Processual civil")
+receber_nome_usuario = input("Digite seu nome: ")
+
+print(f"Vamos começar {receber_nome_usuario}")
