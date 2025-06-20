9cc565ae3d607d4d00865d2852ea1afba0b6dc11
JonWilk365 <jonataswilkinson@gmail.com>
2025-06-20 13:11:18 -0300

    implementação primeira pergunta com score

--- quiz.py
+++ quiz.py
@@ -2,3 +2,14 @@ print("Seja bem-vindo ao Quiz de Direito Constitucional, Direito Civil e Process
 receber_nome_usuario = input("Digite seu nome: ")
 
 print(f"Vamos começar {receber_nome_usuario}")
+
+receber_idade = input("Qual é a sua idade: ")
+score = 0
+
+if receber_idade == '18':
+    print("Certa Resposta!")
+    score = score + 1
+else:
+    print("Resposta Incorreta!")
+
+print(f"Sua pontuação: {score}")  
